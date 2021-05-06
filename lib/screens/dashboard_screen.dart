f13feca348dda216687a615d7aebefe2befba3f9
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2021-05-07 01:23:14 +0530

    dashboard_screen.dart ui completed

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -15,62 +15,122 @@ class _DashboardScreenState extends State<DashboardScreen> {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Food',style: GoogleFonts.roboto(color: Theme.of(context).primaryColor,fontWeight: FontWeight.w700),),
-            Text(' Explorer',style: GoogleFonts.roboto(color: Colors.black,fontWeight: FontWeight.w700),),
+            Text(
+              'Food',
+              style: GoogleFonts.roboto(
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w700),
+            ),
+            Text(
+              ' Explorer',
+              style: GoogleFonts.roboto(
+                  color: Colors.black, fontWeight: FontWeight.w700),
+            ),
           ],
         ),
         centerTitle: true,
         actions: [
           Container(
-            padding: EdgeInsets.all(5),
-            child: Icon(
+              padding: EdgeInsets.all(5),
+              child: Icon(
                 Icons.view_headline,
-              color: Colors.black,
-              size: 30,
-            )
-            ),
-
+                color: Colors.black,
+                size: 30,
+              )),
         ],
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
-            padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.grey)
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey)),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.search_outlined,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Panner Tikka',
+                    style: GoogleFonts.roboto(fontSize: 18, color: Colors.grey),
+                  ),
+                ],
+              ),
             ),
-            child: Row(
-              children: [
-                Icon(
-                    Icons.search_outlined,color: Colors.grey,
-                ),
-                SizedBox(width: 10,),
-                Text('Panner Tikka',style: GoogleFonts.roboto(fontSize: 18,color: Colors.grey),),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 20),
-            width: MediaQuery.of(context).size.width,
-              child: Text(
-                'Eat what makes you happy',style: GoogleFonts.roboto(fontSize: 16),)
-          ),
-        ],
+            Container(
+                padding: EdgeInsets.only(left: 20),
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  'Eat what makes you happy',
+                  style: GoogleFonts.roboto(fontSize: 22),
+                )),
+            SizedBox(height: 10,),
+            GridView.count(
+              physics: ScrollPhysics(),
+              shrinkWrap: true,
+              crossAxisCount: 3,
+              children: List.generate(10, (index) {
+                return CategoryCard(name: 'batman 1',imageUrl: 'https://i.ytimg.com/vi/NLOp_6uPccQ/maxresdefault.jpg');
+              }),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
 class CategoryCard extends StatelessWidget {
+
+  final String name;
+  final String imageUrl;
+  CategoryCard({this.name, this.imageUrl});
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.all(5),
+      width: MediaQuery.of(context).size.width / 3.2,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.grey)),
+      child: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
 
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100.0),
+                  child: Image.network(
+                    imageUrl,
+                    fit: BoxFit.fill,
+                    width: MediaQuery.of(context).size.width /4.2,
+                    height: MediaQuery.of(context).size.width / 4.2,
+                  ),
+                ),
+              ),
+              SizedBox(height: 2,),
+              Text(name,style: GoogleFonts.roboto(fontSize: 15),)
+            ],
+          ),
+          Align(
+              alignment: Alignment.topRight,
+              child: Icon(Icons.bookmark_border,size: 25,color: Colors.grey,)
+          )
+        ],
+      ),
     );
   }
 }
-
